4d1f45d44bc829d2293c1eba0340371f8105955c
ETHAN MC DONNELL <G00355252@gmit.ie>
2018-12-18 17:26:52 +0000

    UI initialised, temperature and  currency pages set up and calculations finished.

---
+++ McConvert/McConvert/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace McConvert
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+            async void Convert_Temperature(object sender, EventArgs args)
+            {
+                await Navigation.PushAsync(new Temperature());
+            }
+
+            async void CurrencyConvert(object sender, EventArgs args)
+            {
+                await Navigation.PushAsync(new Temperature());
+            }
+
+            async void SpeedConvert(object sender, EventArgs args)
+            {
+                await Navigation.PushAsync(new Temperature());
+            }
+
+            async void StorageConvert(object sender, EventArgs args)
+            {
+                await Navigation.PushAsync(new Temperature());
+            }
+
+            async void VolumeConvert(object sender, EventArgs args)
+            {
+                await Navigation.PushAsync(new Temperature());
+            }
+
+            async void WeightConvert(object sender, EventArgs args)
+            {
+                await Navigation.PushAsync(new Temperature());
+            }
+
+            async void TimeConvert(object sender, EventArgs args)
+            {
+                await Navigation.PushAsync(new Temperature());
+            }
+
+            async void LengthConvert(object sender, EventArgs args)
+            {
+                await Navigation.PushAsync(new Temperature());
+            }
+        
+
+    }
+    }
+
---
+++ McConvert/McConvert/Temperature.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace McConvert
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Temperature : ContentPage
+	{
+		public Temperature ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double C = Convert.ToDouble(Celcius.Text);
+            double F = Convert.ToDouble(Fahrenheit.Text);
+
+            F = (C * 9/5) + 32;
+
+            FahrOut.Text = "The temperature is: " + F;
+        }
+    }
+}
